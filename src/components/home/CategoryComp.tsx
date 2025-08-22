1dda0307ce1c73c356fd22b5399ceca9fc32a0b0
iysaofficial <youngscientist.iysa@gmail.com>
2025-08-22 10:56:08 +0700

    first

---
+++ src/components/home/CategoryComp.tsx
@@ -0,0 +1,28 @@
+const CategoryComp = () =>{
+    return(
+        <>
+        <section className="category-section" id="category">
+            <div className="category-container">
+                <br />
+                <br />
+                <br />
+                <div className="serviceBox">
+                    <div className="icon">
+                        <img src="/assets/images/icon/Senior High School.png" alt="" />
+                    </div>
+                    <div className="content">
+                        <h2>Senior High School</h2>
+                        <p>Come enter your school in the economy olympiad and achieve achievements together</p>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <br />
+             
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default CategoryComp
